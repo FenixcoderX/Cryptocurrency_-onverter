dea8eaca299cb73c19684bfce4841cee56afa5ed
FenixcoderX <fenixcoder2024@gmail.com>
2024-07-08 10:29:53 +0700

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center ">
+        App
+      </div>
+    </>
+  );
+};
+
+export default App;
